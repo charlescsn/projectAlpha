881800ba2b64f86f9cdf4732a317fb2515f87f71
rqphy <raphael.ferreira@hetic.net>
2021-04-16 21:59:34 +0200

    add email check

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+
 const bcrypt = require('bcrypt');
+const validator = require("email-validator");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -20,17 +22,24 @@ app.get('/', (req, res) => {
 })
 
 app.post('/api/user/create', (req, res) => {
+    
+    
     const newUser = {
-        PLATeFORME_ID: req.body.platId,
+        PLATEFORME_ID: req.body.platId,
         USERNAME: req.body.username,
-        EMAIL: req.body.email,
+        EMAIL: null,
         PWD: req.body.password,
         PHONENUMBER: req.body.phoneNumber,
         JOINED_DATE: req.body.joinedDate,
         BIRTHDATE: req.body.birthdate,
         COUNTRY: req.body.country
     }
-
+    
+    if (validator.validate(req.body.email)) {
+        newUser.EMAIL = req.body.email;
+    } else {
+        res.send('E.M.A.I.L troudbal, le truc avec un @test.fr');
+    }
     
 
     bcrypt.hash(newUser.PWD, 10).then(async function(hash) {
